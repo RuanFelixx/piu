472bd5575844c1f6d89581ce4a68817396625d6c
Ruan Allyson de Araújo Felix <20221101110080@ifrn.local>
2025-05-07 17:53:59 -0300

    add aula01

---
+++ piu - aula01/doc.md
@@ -0,0 +1,9 @@
+#comandos
+node -v
+npm -v
+npm create vite@latest
+cd vite-project
+npm install
+npm run dev
+
+lembrando que aparecera as opções pra comfirmar "y", react, js
---
+++ piu - aula01/vite-project/src/MyFirstComponent.jsx
@@ -0,0 +1,17 @@
+function MyFirstComponent(){
+    const nome = "Ruan"
+
+    return(
+        <>
+            <div>
+                <h1>Hellow world!!!!</h1>
+            </div>
+            <div>
+                <p>Seu nome é{nome}</p>
+                <p>A soma de react + html = choro coletivo</p>
+            </div>
+        </>
+    )
+}
+
+export default MyFirstComponent
