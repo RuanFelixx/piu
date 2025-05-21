3e08eceab69edd1c50271dd5551d9a453daf1672
Ruan Allyson de Araújo Felix <20221101110080@ifrn.local>
2025-05-21 17:34:44 -0300

    add teste

---
+++ piu - teste02/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Desafio from '' 
+
+function App() {
+
+
+  return (
+    <>
+      <Desafio />
+    </>
+  )
+}
+
+export default App
