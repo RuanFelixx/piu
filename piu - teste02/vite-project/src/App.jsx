a5ab6f066631b6b5c7c4c0ed711cfb3914ecb4a2
RuanFelixx <ruan.felix2024@gmail.com>
2025-05-22 11:21:11 -0300

    Desafio de Iuri

--- piu - teste02/vite-project/src/App.jsx
+++ piu - teste02/vite-project/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Desafio from '' 
+import Desafio from './componentes/Desafio.jsx';
+
 
 function App() {
 
---
+++ piu - teste02/vite-project/src/componentes/Desafio.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import './Desafio.css';
+import arlon from "../assets/arlon.jpeg";
+import hiandro from "../assets/hiandro.jpeg";
+import teo from "../assets/teo.jpeg";
+import brian from "../assets/brian.jpeg";
+import dudu from "../assets/dudu.jpeg";
+
+function Desafio() {
+  const [backgroundColor, setBackgroundColor] = useState('white');
+  const [selecionado, setSelecionado] = useState(null);
+
+  function handleClick() {
+    const newColor = backgroundColor === 'white' ? 'black' : 'white';
+    setBackgroundColor(newColor);
+    document.body.style.backgroundColor = newColor;
+    document.body.style.color = newColor === 'white' ? 'black' : 'white';
+  }
+
+  const personagens = [
+    {
+      nome: "Arlon",
+      img: arlon,
+      descricao: "Arlon chico pleto, promessa de jardin de piranhas e assasino da altinha.",
+    },
+    {
+      nome: "Hiandro",
+      img: hiandro,
+      descricao: "Hiandro 14 polegadas, seria da 2v e programador nato",
+    },
+    {
+      nome: "Teo",
+      img: teo,
+      descricao: 'Teo o "homem" que criou o chatgpt',
+    },
+    {
+      nome: "Brian",
+      img: brian,
+      descricao: 'Brian o peladeiro, inimigo das aulas e comparsa de Ayslan',
+    },
+    {
+      nome: "Dudu",
+      img: dudu,
+      descricao: 'Dudu o "homem" mais manicaca do if',
+    },
+  ];
+
+  return (
+    <div className="conteudo">
+      <button onClick={handleClick}>Altere o tema</button>
+      <h2>Escolha seu personagem do elenco das estrelas</h2>
+      <div className="personagens">
+        {personagens.map((p, index) => (
+          <img
+            key={index}
+            src={p.img}
+            alt={p.nome}
+            onClick={() => setSelecionado(p)}
+          />
+        ))}
+      </div>
+
+      {selecionado && (
+        <div className="selecionado">
+          <h3>{selecionado.nome}</h3>
+          <img
+            src={selecionado.img}
+            alt={selecionado.nome}
+          />
+          <p>{selecionado.descricao}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Desafio;
